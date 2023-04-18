0e74c6199010973acfb7c1b2e2b3e3370c489196
R-K-96 <124651427+R-K-96@users.noreply.github.com>
2023-04-18 10:36:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 function getFormvalue() {
     //Write your code here
+	 var firstName = document.getElementById("fName").value;
+    var lastName = document.getElementById("lName").value;
+
+    // Display the full name using alert()
+    alert( firstName +" "+ lastName);
 
 }
